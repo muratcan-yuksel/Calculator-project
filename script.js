8442dfe2d926b74c2dc0f039e8bcccc6a054a554
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-26 15:21:41 +0300

    added the 4 necessary functions + the operate function

---
+++ script.js
@@ -0,0 +1,19 @@
+let addition = function (x, y){
+return x + y;
+}
+let subtraction = function (x, y) {
+    return x - y;
+}
+let multiplication = function (x,y) {
+    return x * y;
+}
+let division = function (x,y){
+    return x / y;
+}
+
+let operate = function (x,y,z) {
+if (x == "+") {
+   return addition(y,z);
+}
+
+}
