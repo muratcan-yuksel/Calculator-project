165b86ef142b59231b00c624de990f95409471de
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-30 12:20:16 +0300

    right now, the subtraction works as long as I press the equlas button each and every time. SO, I cannot chain operations at the moment

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let multiplication = function (x,y) {
 let division = function (x,y){
     return x / y;
 }
+
 // a function that calls the operation functions 
 let operate = function (x,y,z) {
 if (x == "+") {
@@ -32,41 +33,42 @@ let display = document.getElementById("display");
 let digits = document.querySelectorAll(".digits");
 for (let i= 0; i< digits.length; i++){
    digits[i].addEventListener("click", function(e) {
-  let displayValue= display.textContent+= e.target.textContent;
+       let displayValue= display.textContent+= e.target.textContent;
+         //save the display value
+  
+
    console.log (displayValue);
-   
+  
    });
 }
 
+
+
 //get the operation buttons
-let addition = document.getElementsByClassName("addition");
-let subtraction = document.getElementsByClassName("subtraction");
-let multiplication = document.getElementsByClassName("multiplication");
-let division = document.getElementsByClassName("division");
+let additionButton = document.getElementById("addition");
+let subtractionButton = document.getElementById("subtraction");
+let multiplicationButton = document.getElementById("multiplication");
+let divisionButton = document.getElementById("division");
 
 
 
+subtractionButton.addEventListener("click", function (e){
+    localStorage.setItem("dispNumber", display.textContent);
+    //empty display area
+ display.textContent="";
 
+ 
+ })
 
-
-
-
-
-
-/*
-document.getElementById("one").addEventListener("click", function(e){
-
-    let displayValue= display.textContent+= e.target.textContent;
-    console.log (displayValue);
-
-})
-document.getElementById("two").addEventListener("click", function(e){
-
-    let displayValue= display.textContent+= e.target.textContent;
-    console.log (displayValue);
+//equals event listener
+let equalsTo= document.getElementById("equals");
+//makes a subtraction of the two given numbers
+equalsTo.addEventListener("click", function(e){
+    display.textContent= (localStorage.getItem("dispNumber") - display.textContent );
 
 })
-*/
+
+
 
 
 
